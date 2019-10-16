ef3e7b6db7b43247e949feb8823e92240f9969f5
miku86 <miku86coding@gmail.com>
2019-10-16 16:34:41 +0200

    add method to write data into file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+/* eslint camelcase: ["error", {allow: ["body_markdown", "published_at"]}] */
 const axios = require('axios').default;
+const fs = require('fs');
 require('dotenv').config();
 
 const API_URL = 'https://dev.to/api';
-const PATH_ALL_ARTICLES = '/articles/me/all?per_page=1&page=1';
+const PATH_ALL_ARTICLES = '/articles/me/all';
 const URL_ALL_ARTICLES = `${API_URL}${PATH_ALL_ARTICLES}`;
 const LENGTH_OF_TIMESTAMP = -14;
+const OUT_DIR = 'OUTPUT/';
 
 const getAllArticles = async (url) => {
   try {
@@ -28,11 +31,26 @@ const createFileName = ({ published_at, slug }) => {
 
 const extractContent = ({ body_markdown }) => (body_markdown);
 
+const writeContentToFile = (fileName, content) => {
+  fs.mkdir(OUT_DIR, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.writeFile(`${OUT_DIR}${fileName}`, content, (err) => {
+        if (err) {
+          throw new Error(err);
+        }
+      });
+    }
+  });
+};
+
 const articlesDownloader = async () => {
   const rawArticles = await getAllArticles(URL_ALL_ARTICLES);
   rawArticles.forEach((article) => {
     const fileName = createFileName(article);
     const content = extractContent(article);
+    writeContentToFile(fileName, content);
   });
 };
 
